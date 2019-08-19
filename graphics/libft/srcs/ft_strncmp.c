3c2e2bbe5243d3ef1a070062328cc81689589e3d
Jeongmo BAE <jebae@e1r3p16.42.fr>
2019-08-19 18:12:57 +0200

    add libft all files

---
+++ graphics/libft/srcs/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jebae <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/05 19:58:48 by jebae             #+#    #+#             */
+/*   Updated: 2019/04/05 20:01:48 by jebae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+		return (0);
+	while (*s1 != '\0' && --n != 0)
+	{
+		if (*((unsigned char *)s1) != *((unsigned char *)s2))
+			return (*((unsigned char *)s1) - *((unsigned char *)s2));
+		s1++;
+		s2++;
+	}
+	return (*((unsigned char *)s1) - *((unsigned char *)s2));
+}
