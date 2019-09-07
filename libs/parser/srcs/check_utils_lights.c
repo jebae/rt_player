1598c8edad8c59c5b37ee40fa70f11e61731405c
Alexandru MORARU <almoraru@e1r5p10.42.fr>
2019-09-07 18:36:25 +0200

    added default value for light intensity in case it's missing, added more tests scenes and renormed parser. So far no leaks or errors :)

--- libs/parser/srcs/check_utils_lights.c
+++ libs/parser/srcs/check_utils_lights.c
@@ -6,7 +6,7 @@
 /*   By: almoraru <almoraru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/31 15:14:02 by almoraru          #+#    #+#             */
-/*   Updated: 2019/08/31 15:16:28 by almoraru         ###   ########.fr       */
+/*   Updated: 2019/09/07 18:13:40 by almoraru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ void	check_number_of_spherical_light(t_utils *u, char *str)
 
 	nb = &u->nb;
 	if (nb->origin != 1)
-		ft_error_parse(u, str, "Wrong plane parameters!");
+		ft_error_parse(u, str, "Wrong spherical light parameters!");
 	ft_bzero(nb, sizeof(nb) * 10);
 }
 
-void	check_number_of_directional_light(t_utils *u, char *str)
+void	check_number_of_distant_light(t_utils *u, char *str)
 {
 	t_num *nb;
 
 	nb = &u->nb;
 	if (nb->d != 1)
-		ft_error_parse(u, str, "Wrong plane parameters!");
+		ft_error_parse(u, str, "Wrong distant light parameters!");
 	ft_bzero(nb, sizeof(nb) * 10);
 }
